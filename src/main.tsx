e0d99c20b85b2cf30c3b848ca7297b90f71b6c48
sashko-kapillar <kapillarua@gmail.com>
2025-04-06 07:42:49 +0300

    install RTK & create store.ts + delete store/.gitkeep + create branch "redux"

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root")!;
+
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
