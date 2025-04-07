1a7c1e85ecbe0c5ffe0c0464c7fdf73ec9dccb99
Yuli <yu.expert@gmail.com>
2025-04-07 23:23:43 +0300

    feat: setup routing structure with MainLayout and lazy-loaded pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import NotFoundPage from "../../pages/NotFound/NotFoundPage";
+
+const HomePage = lazy(() => import("../../pages/Home/HomePage"));
+const CatalogPage = lazy(() => import("../../pages/Catalog/CatalogPage"));
 
 function App() {
-  return <div>Welcome to Home Page.</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
