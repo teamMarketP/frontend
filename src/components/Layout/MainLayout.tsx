5cdb24b7e621938bc9874ea80cb87e44740d5dd2
Kateryna <katerynawalls@proton.me>
2025-04-07 18:35:18 -0500

    fix: resolve merge conflicts and add footer

---
+++ src/components/Layout/MainLayout.tsx
@@ -0,0 +1,23 @@
+
+
+import { Outlet } from "react-router-dom";
+import Footer from "../App/Footer/Footer";
+import Header from "../App/Header/Header";
+import { Suspense } from "react";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer />
+    </>
+  );
+
+};
+
+export default MainLayout;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root")!;
+
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
