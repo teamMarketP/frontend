f19c9d9cbca8d30e75f214bba6157835ff79a914
Yuli <yu.expert@gmail.com>
2025-04-12 20:00:45 +0300

    fix: style cursor for disabled

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,12 @@
+import Button from '@/components/Ui/Button/Button';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Button type="button" label="Пошук" />
+      <Button type="submit" label="Google Auth" disabled />
+    </div>
+  );
 };
 
 export default HomePage;
