0e32f6b77d7f831f3b6a788481460890abb6da8d
Yuli <yu.expert@gmail.com>
2025-04-17 22:57:05 +0300

    style: add btn-component style, add new colors

--- src/components/Ui/Button/Button.tsx
+++ src/components/Ui/Button/Button.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 type ButtonProps = {
   label: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
-  type?: 'button' | 'submit' | 'reset';
+  type: 'button' | 'submit' | 'reset';
   disabled?: boolean;
   className?: string; // Проп для передачі кастомних класів
 };
@@ -17,11 +17,9 @@ const Button: FC<ButtonProps> = ({
   className,
 }) => {
   const buttonClass = clsx(
-    'min-w-[148px] min-h-[36px] p-[8px] bg-tenn text-alabaster rounded-[10px] ', // Загальні стилі для всіх кнопок
+    'btn', // Загальні стилі для всіх кнопок
     {
-      'cursor-pointer hover:bg-tenn/90 transition ease-in-out duration-300':
-        !disabled,
-      'cursor-not-allowed opacity-50': disabled,
+      'btn-disabled': disabled,
     },
     className
   );
