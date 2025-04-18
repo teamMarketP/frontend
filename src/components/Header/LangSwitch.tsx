b9507b3bab7e934cad0ff5b3f71e9fe39077487c
sashko-kapillar <kapillarua@gmail.com>
2025-04-18 18:13:08 +0300

    add hover for switch

--- src/components/Header/LangSwitch.tsx
+++ src/components/Header/LangSwitch.tsx
@@ -3,7 +3,7 @@ export const LangSwitch = () => {
     <div className="text-sm font-semibold text-fire space-x-1">
       <span className="text-black">EN</span>
       <span className="text-fire">|</span>
-      <span className="underline cursor-pointer">UA</span>
+      <span className="underline cursor-pointer hover:text-sky-700">UA</span>
     </div>
   );
 };
