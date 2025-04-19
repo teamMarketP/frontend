58732f87324d53fb4eb7a7af0945ea432c88c178
Kateryna <katerynawalls@proton.me>
2025-04-18 21:57:31 -0500

    feat: update footer layout according to new design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,103 +1,75 @@
 import sprite from '@/assets/sprite.svg';
-
-const footerSections = [
-  {
-    title: 'Про нас',
-    items: ['Про проєкт', 'Контакти'],
-  },
-  {
-    title: 'Як це працює',
-    items: ['Як замовити послугу'],
-  },
-  {
-    title: 'Допомога',
-    items: [
-      'Питання та відповіді',
-      'Правила конфіденційності',
-      'Служба підтримки',
-      'Публічна оферта',
-    ],
-  },
-  {
-    title: 'Посилання',
-    items: ['Всі категорії послуг', 'Популярні послуги', 'Топ спеціалістів'],
-  },
-];
-
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-t from-[#6d2e01] to-[#ce5601] text-white rounded-t-[60px] w-full">
-      <div className="flex max-w-[1280px] mx-auto justify-between items-start gap-[60px] px-[120px] mb-[50px]">
-        {/* Ліва частина */}
-        <div className="flex gap-[40px]">
-          {footerSections.slice(0, 2).map(({ title, items }, index) => (
-            <div key={index} className="pt-[61px]">
-              <h3 className="font-semibold mb-[15px] text-xl">{title}</h3>
-              <ul className="leading-[2.5] text-base font-normal text-alabaster font-primary">
-                {items.map((item, i) => (
-                  <li key={i}>
-                    <a href="#">{item}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
+    <footer className="bg-gradient-to-t from-[#6d2e01] to-[#ce5601] text-white rounded-t-[60px] w-full  pt-[29px] pr-[121px] pb-[25px] pl-[119px] flex flex-col items-center  max-w-[1280px] mx-auto ">
+      {/* Логотип */}
+      <div className="flex items-end justify-center h-[38px] mb-[39px]">
+        <p className="text-base font-third text-alabaster leading-none">PETS</p>
+        <svg className="w-[61px] h-[39px] fill-[#f9f9f9] mx-[4px]">
+          <use xlinkHref={`${sprite}#icon-logo`} />
+        </svg>
+        <p className="text-base font-third text-alabaster leading-none">HELP</p>
+      </div>
 
-        {/* Центр: Логотип і соцмережі */}
-        <div className="flex flex-col items-center pt-[43px]">
-          {/* Логотип */}
-          <div className="flex items-end justify-center h-[38px] mb-[73px]">
-            <p className="text-lg font-normal font-third text-alabaster leading-none">
-              PETS
-            </p>
-            <svg className="w-[59px] h-[38px] fill-[#f9f9f9]">
-              <use xlinkHref={`${sprite}#icon-logo`} />
-            </svg>
-            <p className="text-lg font-normal font-third text-alabaster leading-none">
-              HELP
-            </p>
-          </div>
+      {/* 4 колонки */}
+      <div className="flex gap-[138px] mb-[31px]">
+        <ul>
+          <li className="font-semibold text-[20px] mb-[10px] text-alabaster font-primary">
+            Про нас
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Про проєкт</a>
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Контакти</a>
+          </li>
+        </ul>
 
-          {/* Соцмережі */}
-          <div className="flex gap-[29px] flex-row justify-start items-center">
-            <a href="#" aria-label="Viber">
-              <svg className="w-[31px] h-[31px] fill-alabaster">
-                <use xlinkHref={`${sprite}#icon-viber`} />
-              </svg>
-            </a>
-            <a href="#" aria-label="Instagram">
-              <svg className="w-[27px] h-[26px] fill-alabaster">
-                <use xlinkHref={`${sprite}#icon-instagram`} />
-              </svg>
-            </a>
-            <a href="#" aria-label="Facebook">
-              <svg className="w-[26px] h-[25px] fill-alabaster">
-                <use xlinkHref={`${sprite}#icon-facebook`} />
-              </svg>
-            </a>
-          </div>
-        </div>
+        <ul>
+          <li className="font-semibold text-[20px] mb-[10px] text-alabaster font-primary">
+            Допомога
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Питання та відповіді</a>
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Правила конфіденційності</a>
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Служба підтримки</a>
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Публічна оферта</a>
+          </li>
+        </ul>
 
-        {/* Права частина */}
-        <div className="flex gap-[40px]">
-          {footerSections.slice(2).map(({ title, items }, index) => (
-            <div key={index} className="pt-[61px]">
-              <h3 className="font-semibold mb-[15px] text-xl">{title}</h3>
-              <ul className="leading-[2.5] text-base font-normal text-alabaster font-primary">
-                {items.map((item, i) => (
-                  <li key={i}>
-                    <a href="#">{item}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
+        <ul>
+          <li className="font-semibold text-[20px] mb-[10px] text-alabaster font-primary">
+            Як це працює
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Як замовити послугу?</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li className="font-semibold text-[20px] mb-[10px] text-alabaster font-primary">
+            Посилання
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Вигул</a>
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Підтримка</a>
+          </li>
+          <li className="font-normal text-[16px] leading-[2.5] text-alabaster font-primary">
+            <a href="#">Грумінг</a>
+          </li>
+        </ul>
       </div>
 
       {/* Копірайт */}
-      <p className="text-center font-primary text-xs text-alabaster pb-5">
+      <p className="font-normal text-[12px]  font-primary text-alabaster  text-center">
         &copy; 2025 PetsHelp marketplace
       </p>
     </footer>
