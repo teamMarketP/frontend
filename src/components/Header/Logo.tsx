3c38bc706d300b3f485d239fc5d3c2f549e0cd51
sashko-kapillar <kapillarua@gmail.com>
2025-04-20 00:01:12 +0300

    fix logo position

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,12 +1,11 @@
 export const Logo = () => {
   return (
-    <a href="#" className="flex items-end gap-1 ">
-      <span className="text-fire font-bold text-lg  relative top-1">PETS</span>
-      {/* <img src="/icons.svg#icon-logo" alt="PetsHelp" className="h-8 w-auto" /> */}
-      <svg className="w-[60px] h-[40px] fill-fire ">
+    <a href="#" className="flex items-baseline">
+      <span className="text-fire font-bold text-lg">PETS</span>
+      <svg className="w-[60px] h-[40px] fill-fire relative top-[1px]">
         <use href="/icons.svg#icon-logo" />
       </svg>
-      <span className="text-fire font-bold text-lg relative top-1">HELP</span>
+      <span className="text-fire font-bold text-lg">HELP</span>
     </a>
   );
 };
