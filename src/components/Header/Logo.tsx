5c614b5641fce9b2387d4682560b920b56099a7a
sashko-kapillar <kapillarua@gmail.com>
2025-04-20 00:11:29 +0300

    change <a> on <Link>

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export const Logo = () => {
   return (
-    <a href="#" className="flex items-baseline">
+    <Link to="#" className="flex items-baseline">
       <span className="text-fire font-bold text-lg">PETS</span>
       <svg className="w-[60px] h-[40px] fill-fire relative top-[1px]">
         <use href="/icons.svg#icon-logo" />
       </svg>
       <span className="text-fire font-bold text-lg">HELP</span>
-    </a>
+    </Link>
   );
 };
