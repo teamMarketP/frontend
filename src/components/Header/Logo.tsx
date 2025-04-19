06154869364bce9fabd7aaaadef32a1543b03997
sashko-kapillar <kapillarua@gmail.com>
2025-04-19 20:16:44 +0300

    add logo.svg

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,9 +1,12 @@
 export const Logo = () => {
   return (
-    <a href="#" className="flex items-center gap-1">
-      <span className="text-fire font-bold text-lg">PETS</span>
-      <img src="/logo.svg" alt="PetsHelp" className="h-8 w-auto" />
-      <span className="text-fire font-bold text-lg">HELP</span>
+    <a href="#" className="flex items-end gap-1 ">
+      <span className="text-fire font-bold text-lg  relative top-1">PETS</span>
+      {/* <img src="/icons.svg#icon-logo" alt="PetsHelp" className="h-8 w-auto" /> */}
+      <svg className="w-[60px] h-[40px] fill-fire ">
+        <use href="/icons.svg#icon-logo" />
+      </svg>
+      <span className="text-fire font-bold text-lg relative top-1">HELP</span>
     </a>
   );
 };
