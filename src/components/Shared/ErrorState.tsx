aa60422b6d07a5844461f7c931301e08de20a337
Kateryna <katerynawalls@proton.me>
2025-04-25 23:12:53 -0500

    feat(PT-47): add animated ErrorState with dog in water and Button

---
+++ src/components/Shared/ErrorState.tsx
@@ -0,0 +1,32 @@
+import Player from 'lottie-react';
+import errorAnimation from '@/assets/animations/dog-in-water-errorstate.json';
+import { useNavigate } from 'react-router-dom';
+import Button from '@/components/Ui/Button/Button';
+
+const ErrorState = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-20 px-4">
+      {/* Lottie анімація */}
+      <Player
+        autoplay
+        loop
+        animationData={errorAnimation}
+        className="w-[220px] h-[220px]"
+      />
+
+      <div className="text-center text-cod-gray text-lg mt-6 mb-6">
+        Щось пішло не так. Спробуйте ще раз пізніше.
+      </div>
+
+      <Button
+        label="Повернутись до пошуку"
+        type="button"
+        onClick={() => navigate('/')}
+      />
+    </div>
+  );
+};
+
+export default ErrorState;
