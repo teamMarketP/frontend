8fb34918f8dc7518a2051510aec3cf711e6fd53b
kapillar <kapillarua@gmail.com>
2025-04-27 08:03:59 +0300

    style: change slyle in HeroBlob/

--- src/components/Hero/HeroBlob.tsx
+++ src/components/Hero/HeroBlob.tsx
@@ -1,11 +1,11 @@
   const DogWithBlob = () => {
     return (
       <div className="relative">
-        <div className="absolute rotate-[145deg] bg-white rounded-[265px] border-[2px] border-fire shadow-2xlz-0" />
+        <div className="absolute rotate-[145deg] rounded-[265px] border-[2px] border-fire shadow-2xl" />
         <img
           src="/images/blob-behind-dog.png"
-          alt="Dog"
-          className="relative z-10 w-[auto] h-[auto] h-auto -top-196 -right-132 "
+          alt="Rectangle"
+          className="relative z-10 w-auto h-auto -top-196 -right-132 "
         />
       </div>
     );
