2cd2208df0626dbea8fb45cbf74b389fe85bd624
kapillar <kapillarua@gmail.com>
2025-04-27 08:12:24 +0300

    fix: delete HeroContent file & change Content to Title in HeroSection

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -1,14 +1,14 @@
-// src/components/Hero/Hero.tsx
-import HeroContent from '../Hero/HeroContent';
+
 import HeroImage from '../Hero/HeroImage';
 import HeroFilters from '../Hero/HeroFilters';
 import DogWithBlob from '../Hero/HeroBlob';
+import HeroTitle from '../Hero/HeroTitle';
 
 const HeroSection = () => {
   return (
     <section className="py-12 bg-gradient-to-bl from-red-beech via-tenn to-red-beech w-full  rounded-b-[40px] max-w-[1280px] mx-auto -mt-20 px-30 max-h-165 mb-43">
       <div className="container relative mx-auto flex flex-col-reverse lg:flex-row items-center gap-10">
-        <HeroContent />
+        <HeroTitle />
         <HeroImage />
       </div>
       <div className="absolute z-0">
