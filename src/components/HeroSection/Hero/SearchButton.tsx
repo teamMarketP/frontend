657700cb1cd48d54752f2ae05f100f51ea8b8d7d
kapillar <kapillarua@gmail.com>
2025-04-29 21:53:33 +0300

    feat(23): add new logic in SerchBtn

--- src/components/HeroSection/Hero/SearchButton.tsx
+++ src/components/HeroSection/Hero/SearchButton.tsx
@@ -1,27 +1,27 @@
 import RegisterForm from '@/components/RegisterForm/RegisterForm';
 import Modal from '@/components/Ui/Modal/Modal';
+import Button from '@/components/Ui/Button/Button';
 import { useState } from 'react';
 
 const SearchButton = () => {
   const [openRegisterModal, setOpenRegisterModal] = useState(false);
-    return (
-      <>
-      <button
-      className="btn flex flex-1 items-center justify-center text-[20px] max-w-[304px] h-auto px-6 py-4 rounded-[16px] hover:shadow-shark active:shadow-inset-shark"
-      type="button"
-      aria-label="Пошук"
-        onClick={() => setOpenRegisterModal(true)}>
-        Пошук
-      </button>
+
+  return (
+    <>
+      <Button
+        label="Пошук"
+        type="button"
+        onClick={() => setOpenRegisterModal(true)}
+        className="flex flex-1 items-center justify-center text-[20px] max-w-[304px] h-auto px-6 py-4 rounded-[16px] hover:shadow-shark active:shadow-inset-shark"
+      />
       <Modal
-      isOpen={openRegisterModal}
-      onClose={() => setOpenRegisterModal(false)}
-    >
-      <RegisterForm />
-    </Modal>
+        isOpen={openRegisterModal}
+        onClose={() => setOpenRegisterModal(false)}
+      >
+        <RegisterForm />
+      </Modal>
     </>
-    );
-  };
-  
-  export default SearchButton;
-  
+  );
+};
+
+export default SearchButton;
