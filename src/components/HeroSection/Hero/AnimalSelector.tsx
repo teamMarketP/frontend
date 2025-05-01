be6d1ede765d74547ad4ecb1fbdf3b455a1a6ed3
kapillar <kapillarua@gmail.com>
2025-05-02 01:12:29 +0300

    style(pt-23): change shadow to the AnimalSelector

--- src/components/HeroSection/Hero/AnimalSelector.tsx
+++ src/components/HeroSection/Hero/AnimalSelector.tsx
@@ -12,12 +12,12 @@ const AnimalSelector = () => {
   };
 
   return (
-    <div className="flex justify-center rounded-[12px] overflow-hidden bg-wildSand w-1/2 border-[2px] border-tenn hover:shadow-shark">
+    <div className="flex justify-center rounded-[12px] bg-wildSand w-1/2 border-[2px] border-tenn">
       {(["dog", "cat"] as const).map((animal) => (
         <button
           key={animal}
           onClick={() => handleSelect(animal)}
-          className={`px-4 w-1/2 py-3 font-semibold transition  active:shadow-inset-shark ${
+          className={`px-4 w-1/2 py-3 font-semibold transition hover:shadow-shark active:shadow-inset-shark ${
             selectedAnimal === animal
               ? `bg-tenn text-alabaster ${animal === "dog" ? "rounded-l-[8px]" : "rounded-r-[8px]"}`
               : "text-fire"
