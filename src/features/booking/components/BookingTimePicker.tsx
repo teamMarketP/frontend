13b32b987f814dc46d4029e832266dee8809a986
Yuli <yu.expert@gmail.com>
2025-05-08 23:34:57 +0300

    feat(PT-51): add BookingTimePicker layout and styles

--- src/features/booking/components/BookingTimePicker.tsx
+++ src/features/booking/components/BookingTimePicker.tsx
@@ -1,9 +1,33 @@
+import { timeSlots } from '@/features/booking/constants/bookingTime';
+
 const BookingTimePicker = () => {
   return (
     <div>
-      <h4 className="text-xl font-semibold text-fire">
+      <h4 className="xl:text-xl xl:font-semibold text-fire xl:mb-5">
         Час виконання замовлення
       </h4>
+      <ul className="flex gap-2">
+        {timeSlots.map((slot, index) => {
+          return (
+            <li
+              key={index}
+              className="w-[132px] border-2 border-fire rounded-2xl"
+            >
+              <label className="h-10 flex items-center gap-2 leading-none text-sm px-[10px] ">
+                <input
+                  type="radio"
+                  name="time"
+                  value={slot.value}
+                  // checked={selectedOption === {slot.value}}
+                  // onChange={handleChange}
+                  className="book-radio-btn"
+                />
+                {slot.time}
+              </label>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
