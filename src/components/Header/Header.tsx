2c9b2662eddb473d03580d6301c3a33465ed0b00
kapillar <kapillarua@gmail.com>
2025-05-10 23:05:20 +0300

    style(PT-49): change style in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,9 +9,9 @@ const Header: React.FC = () => {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
   return (
-    <header className="relative bg-alabaster z-30 h-[68px] shadow-sm font-primary max-w-[1232px] mx-auto mt-2 rounded-[16px] flex">
+    <header className="relative z-20 h-[68px] font-base max-w-[1232px] shadow-box mx-auto mt-2 flex">
       {!isHomePage && <GradientHeaderWrapper />}
-      <div className="container mx-auto px-24 flex items-center justify-between">
+      <div className="relative z-30 bg-alabaster container mx-auto px-24 flex items-center justify-between  rounded-[16px]">
         {/* Left block: Language, Nav*/}
         <div className="flex items-center gap-10">
           <LangSwitch />
