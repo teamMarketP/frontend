663d40dfa6a9fa01ae5a53a6bf22453f83bfbf5c
kapillar <kapillarua@gmail.com>
2025-05-12 00:56:24 +0300

    style(pt-49): add h1 to the page

--- src/features/specialist/pages/SpecialistProfilePage.tsx
+++ src/features/specialist/pages/SpecialistProfilePage.tsx
@@ -5,8 +5,12 @@ import mockProfileData from "../../../data/__mocks__/mockProfileData";
 
 const SpecialistProfilePage = () => {
   return (
-    <div className="mx-auto p-[120px] max-w-[1280px]">
-      {/* <BackButton /> */}
+    <div className="mx-auto px-[120px] pt-37 pb-30 max-w-[1280px]">
+
+      <div className="flex">
+        {/* <BackButton /> */}
+        <h1 className="sr-only text-7xl uppercase">Сторінка фахівця</h1>
+      </div>
       <SpecialistProfileHeader profile={mockProfileData} />
       <SpecialistPetsCarousel images={mockProfileData} />
      <SpecialistReviewsSection />
