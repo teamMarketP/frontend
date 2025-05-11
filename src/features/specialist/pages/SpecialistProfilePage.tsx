a91fffb280133325b93319d1a28f519b7450e22f
kapillar <kapillarua@gmail.com>
2025-05-12 01:15:28 +0300

    style(pt-49): add btn back ( -1 )

--- src/features/specialist/pages/SpecialistProfilePage.tsx
+++ src/features/specialist/pages/SpecialistProfilePage.tsx
@@ -2,14 +2,15 @@ import { SpecialistProfileHeader } from "../components/SpecialistProfileHeader";
 import { SpecialistPetsCarousel } from "../components/SpecialistPetsCarousel";
 import SpecialistReviewsSection from "../components/SpecialistReviewsSection";
 import mockProfileData from "../../../data/__mocks__/mockProfileData";
+import BackButton from "@/shared/utils/UI/BackButton";
 
 const SpecialistProfilePage = () => {
   return (
-    <div className="mx-auto px-[120px] pt-37 pb-30 max-w-[1280px]">
+    <div className="mx-auto px-[120px] pt-17 pb-30 max-w-[1280px]">
 
-      <div className="flex">
-        {/* <BackButton /> */}
-        <h1 className="sr-only text-7xl uppercase">Сторінка фахівця</h1>
+      <div className="flex mb-12">
+        <BackButton />
+        <h1 className="sr-only flex text-5xl uppercase mx-auto text-center">Сторінка фахівця</h1>
       </div>
       <SpecialistProfileHeader profile={mockProfileData} />
       <SpecialistPetsCarousel images={mockProfileData} />
